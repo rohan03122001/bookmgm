3f1d5fef211bbff53470340c747738dae3cd46e9
Rohan Bhujbal <rxb421@student.bham.ac.uk>
2024-12-15 19:49:41 +0000

    controllers build

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,89 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/rohan03122001/bookmgm/pkg/models"
+	"github.com/rohan03122001/bookmgm/pkg/utils"
+)
+
+var NewBook models.Book
+
+
+func GetBook(w http.ResponseWriter, r *http.Request){
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+
+func GetBookById(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	bookId:= vars["bookId"]
+	ID, err := strconv.ParseInt(bookId,0,0)
+	if err != nil{
+		panic(err)
+	}
+
+	bookDetails, _ := models.GetBookById(ID)
+	res,_ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request){
+	 CreateBook := &models.Book{}
+	 utils.ParseBody(r, CreateBook)
+	 b := CreateBook.CreateBook()
+
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)	
+}
+
+
+func DeleteBook(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+
+	id,err := strconv.ParseInt(bookId,0,0)
+	if err != nil{
+		panic(err)
+	}
+
+	book:= models.DeleteBook(id)
+
+	res,_ := json.Marshal(book)
+	w.Header().Set("COntent-Type","pkglication")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request){
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	id, err :=  strconv.ParseInt(bookId,0,0)
+	if err!=nil {
+		fmt.Println("Error While Parsing")
+	}
+
+	bookDetails, db := models.GetBookById(id)
+	if updateBook.Name != ""{
+		bookDetails.Name= updateBook.Name
+	}
+	if updateBook.Author != ""{
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != ""{
+		bookDetails.Publication = updateBook.Publication
+	}
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -25,3 +25,25 @@ func init(){
 }
 
 
+func (b *Book) CreateBook() *Book{
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books 
+} 
+
+func GetBookById(Id int64)(*Book, *gorm.DB){
+	var GetBook Book
+	db := db.Where("ID=?",Id).Find(&GetBook)
+	return &GetBook, db
+}
+
+func DeleteBook(Id int64)(Book){
+	var book Book
+	db.Where("ID=?",Id).Delete(book)
+	return book
+}
